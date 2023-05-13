28a28d3d14424d87d4347c30f94787f3e4d365fc
Joel-Ostos <joelostoscastro@gmail.com>
2023-05-13 01:26:31 -0500

    Practica 2

---
+++ Practica-2-SO/client.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+int main(int argc, char *argv[]){
+  int origin, destination, hod;
+  printf("Bienvenido\n");
+
+  while (1) {
+    printf("1. Ingresar origen\n");
+    printf("2. Ingresar destino\n");
+    printf("3. Ingresar hora\n");
+    printf("4. Buscar tiempo de viaje medio\n");
+    printf("5. Salir\n");
+
+    int option;
+    scanf("%d", &option);
+
+    if (option == 1) {
+      printf("Ingrese ID del origen: ");
+      scanf("%d", &origin);
+    } else if (option == 2) {
+      printf("Ingrese ID del destino: ");
+      scanf("%d", &destination);
+    } else if (option == 3) {
+      printf("Ingrese hora del día: ");
+      scanf("%d", &hod);
+    } else if (option == 4) {
+      int socketfd;
+      struct sockaddr_in server_direccion;
+      struct hostent *server;
+      char buffer[256];
+
+      int numero_puerto = atoi(argv[2]);
+      
+      // Creación del Socket
+      socketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+      // Se evalua si el socket se creó con exito 
+      if (socketfd == -1){
+        printf("Hubo un fallo en la creación del socket");
+        exit(1);
+      }
+
+      server = gethostbyname(argv[1]);
+      if (server == NULL){
+        printf("El server que se indicó no es válido");
+        exit(1);
+      }
+
+      server_direccion.sin_family = AF_INET;
+      bcopy((char *) server->h_addr, (char *) &server_direccion.sin_addr.s_addr, server->h_length);
+      server_direccion.sin_port = htons(numero_puerto);
+      
+      if (connect(socketfd, (struct sockaddr *)&server_direccion, sizeof(server_direccion)) < 0){
+        printf("No se logro conectar con el servidor");
+        exit(1);
+      }
+
+      bzero(buffer, 256);
+      sprintf(buffer, "%d,%d,%d", origin, destination, hod);
+      int n = write(socketfd, buffer, strlen(buffer));
+      if (n < 0) {
+          perror("ERROR writing to socket");
+          exit(1);
+      }
+
+      bzero(buffer, 256);
+      n = read(socketfd, buffer, 255);
+      if (n < 0) {
+          perror("ERROR reading from socket");
+          exit(1);
+      }
+
+      float result;
+      sscanf(buffer, "%f", &result);
+      printf("Received result: %f\n", result);
+
+      close(socketfd);
+    } else if (option == 5) {
+      break;
+    } else {
+      printf("Opción no válida. Intente de nuevo.\n");
+    }
+  }
+
+  return 0;
+}
+
---
+++ Practica-2-SO/server.c
@@ -0,0 +1,107 @@
+// Ficheros de Cabecera
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+typedef struct {
+  int sourceid;
+  int dstid;
+  int hod;
+  float mean_travel_time;
+} TravelData;
+
+void busqueda(int socket) {
+
+float result;
+FILE *file = fopen("prueba.bin", "rb");
+char buffer[256];
+
+TravelData data;
+bzero(buffer, 256);
+int n = read(socket, buffer, 256);
+int hod,dstid,sourceid;
+float mean_travel_time;
+sscanf(buffer, "%d,%d,%d", &sourceid,&dstid,&hod);
+
+while (fread(&data, sizeof(data), 1, file)){
+  if(data.hod == hod && data.dstid == dstid && data.sourceid == sourceid ){
+    mean_travel_time = data.mean_travel_time;
+    break;
+  }else{
+    mean_travel_time = -1;
+  }
+}
+fclose(file);
+
+    // Envía el resultado al cliente.
+    bzero(buffer, 256);
+    sprintf(buffer, "%f", mean_travel_time);
+    n = write(socket, buffer, strlen(buffer));
+    if (n < 0) {
+        perror("ERROR writing to socket");
+        exit(1);
+    }
+}
+
+
+int main(int argc, char *argv[]){
+  int socketfd, socket_cliente, pid;
+  struct sockaddr_in server_direccion, cliente_direccion;
+
+  int numero_puerto = atoi(argv[1]);
+  // argc nos indica la cantidad de parametros que se pasan por terminal, si la cantidad es menor a 2 significa que el puerto no sé especificó
+  if (argc < 2){
+    printf("No se especificó el puerto");
+  }
+
+  // Creación del Socket
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  // Se evalua si el socket se creó con exito xD 
+  if (socketfd == -1){
+    printf("Hubo un fallo en la creación del socket");
+  }
+  
+  // Aquí se modifican los "atributos" de la instancia server_dirección, vease sockaddr para mayor información XD
+  server_direccion.sin_port = htons(numero_puerto);
+  server_direccion.sin_family = AF_INET;
+  server_direccion.sin_addr.s_addr = INADDR_ANY;
+  
+  // Se asigna al socket socketfd la direccion server_dirección, para más tarde hacer la conexión con el cliente
+  if (bind(socketfd, (struct sockaddr *) &server_direccion, sizeof(server_direccion)) == -1){
+    printf("Hubo un error en la asignacion de dirección al socket");
+    exit(-1);
+  }
+
+  listen(socketfd, 5);
+  socklen_t tamano_cliente = sizeof(cliente_direccion);
+  
+  while (1) {
+    socket_cliente = accept(socketfd, (struct sockaddr *) &cliente_direccion, &tamano_cliente);
+
+    if (socket_cliente < 0) {
+      perror("Error al aceptar al cliente");
+      exit(1);
+    }
+    pid = fork();
+    if (pid < 0) {
+      perror("No se logro hacer fork");
+      exit(1);
+    }
+    if (pid == 0) {
+      close(socketfd);
+      busqueda(socket_cliente); // Esto debería ser en el proceso hijo.
+      exit(0);
+    } else {
+      close(socket_cliente); // Esto debería ser en el proceso padre.
+    }
+
+  } 
+  close(socketfd);
+  return 0;
+}
+
